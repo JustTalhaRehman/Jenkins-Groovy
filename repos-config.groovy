7b25883706f9cd0eefcb7244dd036c60c1529c48
JustTalhaRehman <137718375+JustTalhaRehman@users.noreply.github.com>
2026-01-07 17:45:12 +0500

    Update repos-config.groovy

--- repos-config.groovy
+++ repos-config.groovy
@@ -4,7 +4,7 @@ return [
         name: 'product_backend',
         paramName: 'IE_GLOBAL_TAG',
         defaultTag: 'develop',
-        buildCmd: "docker build -t product_backend:develop .",
+        buildCmd: "docker build --t product_backend:develop .",
         description: 'ie-global branch/tag'
     ],
     [
