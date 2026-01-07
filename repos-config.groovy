a1883687e98b9495d956c3faa219c21f56efae3c
JustTalhaRehman <137718375+JustTalhaRehman@users.noreply.github.com>
2026-01-07 17:04:06 +0500

    Update repos-config.groovy

--- repos-config.groovy
+++ repos-config.groovy
@@ -1,16 +1,19 @@
+// repos-config.groovy
 return [
     [
         name: 'product_backend',
         paramName: 'IE_GLOBAL_TAG',
         defaultTag: 'develop',
-        buildCmd: "mvn -U -P '!tag' clean install",
-        description: 'Build backend with Maven'
+        buildCmd: "docker build -t product_frontend: .",
+        description: 'ie-global branch/tag'
     ],
     [
         name: 'product_frontend',
         paramName: 'IE_DEPS_TAG',
         defaultTag: 'develop',
-        buildCmd: "docker build -t product_frontend:!tag .",
-        description: 'Build Docker image for frontend'
+        buildCmd: "docker build -t product_frontend: .",
+        description: 'ie-deps branch/tag'
     ],
+
+    // Add all 16 repos...
 ]
