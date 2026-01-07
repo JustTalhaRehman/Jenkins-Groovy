b75a21df50af3acd4f71a1e0e20412054d730473
JustTalhaRehman <talha32rehman@gmail.com>
2026-01-07 16:28:07 +0500

    update config

--- repos-config.groovy
+++ repos-config.groovy
@@ -14,12 +14,6 @@ return [
         buildCmd: "mvn -U -P '!tag' clean install",
         description: 'ie-deps branch/tag'
     ],
-    [
-        name: 'ie-core',
-        paramName: 'IE_CORE_TAG',
-        defaultTag: 'develop',
-        buildCmd: "mvn clean package -DskipTests",
-        description: 'ie-core branch/tag'
-    ],
+
     // Add all 16 repos...
 ]
